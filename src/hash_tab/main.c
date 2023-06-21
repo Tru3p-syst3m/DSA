245078424e5837d0ddacdcdfbc1276cca7161730
Truep <pvorman54@gmail.com>
2023-06-21 21:40:36 +0700

    correct some mistakes

--- src/hash_tab/main.c
+++ src/hash_tab/main.c
@@ -4,6 +4,6 @@ int main()
 {
 	int answer;
 	printf("Hello! This program demonstrates the work of hash table\n
-	witch type of work you need?\n1)");
-	scanf("")
+	wich type of collision solution do you need?\n1)chaining\n2)open adressing\n");
+	scanf("%d", &answer);
 }
