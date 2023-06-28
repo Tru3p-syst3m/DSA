9e08f5b09181f95de129f891985215919721782b
Truep <pvorman54@gmail.com>
2023-06-28 16:24:02 +0700

    work on main

--- src/hash_tab/main.c
+++ src/hash_tab/main.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 #include <hash_tab.h>
+
+#define SLOTS 16
 
 void chaining_type();
 void open_adressing_type();
 
 int main()
 {
+	srand(time(NULL));
+
 	int answer;
+	
 	printf("Hello! This program demonstrates the work of hash table\n
 	wich type of collision solution do you need?\n1)chaining\n2)open adressing\n");
 	scanf("%d", &answer);
@@ -27,7 +34,11 @@ int main()
 
 void chaining_type()
 {
-
+	hash_tab_chain* hash_tab = init_hash_tab_chain(SLOTS);
+	for(int i = 0; i < SLOTS; i++)
+	{
+		insert_hash_tab_chain(rand(), rand(), hash_tab);
+	}
 }
 
 void open_adressing_type()
